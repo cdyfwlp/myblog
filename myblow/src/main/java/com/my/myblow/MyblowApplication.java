d9f286107434849ec87d6786a5d4d7d84be3caf9
dyf <1@163.com>
2021-06-17 16:43:48 +0800

    '环境搭建'

---
+++ myblow/src/main/java/com/my/myblow/MyblowApplication.java
@@ -0,0 +1,13 @@
+package com.my.myblow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblowApplication.class, args);
+    }
+
+}
